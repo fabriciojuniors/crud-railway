b10a021e9dedefcc3aac156908ddf8921f4423dd
Junior Souza <32983889+fabriciojuniors@users.noreply.github.com>
2023-08-19 11:41:00 -0300

    feat: init application

---
+++ src/main/java/com/fabriciojr/deployrailway/DeployRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciojr.deployrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciojr/deployrailway/DeployRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciojr.deployrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
